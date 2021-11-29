66c6dbe9c29673d386544c1e06497ec31defa03a
simone.dellaria <simonedellaria@gmail.com>
2021-11-29 12:42:06 +0100

    Added project springboot-thymeleaf-hib.

---
+++ springboot-thymeleaf-hib/src/main/java/it/mycompany/springboot/springbootthymeleafhib/SpringbootThymeleafHibApplication.java
@@ -0,0 +1,13 @@
+package it.mycompany.springboot.springbootthymeleafhib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafHibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleafHibApplication.class, args);
+	}
+
+}
---
+++ springboot-thymeleaf-hib/src/test/java/it/mycompany/springboot/springbootthymeleafhib/SpringbootThymeleafHibApplicationTests.java
@@ -0,0 +1,13 @@
+package it.mycompany.springboot.springbootthymeleafhib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafHibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
